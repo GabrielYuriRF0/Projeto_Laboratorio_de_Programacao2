475ad1c02ec234410eb3ecb928e05c2a12734696
GabrielYuriRF0 <gabrie.yuri1020@gmail.com>
2022-08-23 21:39:34 -0300

    Criação do ManageTaskController

---
+++ src/main/java/com/sapo/tasks/ManageTaskController.java
@@ -0,0 +1,26 @@
+package com.sapo.tasks;
+
+public class ManageTaskController {
+    private ManageTaskService manageTaskService;
+
+    public ManageTaskController(ManageTaskService manageTaskService){
+        this.manageTaskService = manageTaskService;
+    }
+
+    public String registerManageTask(String idActivity, String name, String[] skills, String[] idsTasks){
+        return manageTaskService.registerManageTask(idActivity,name,skills,idsTasks);
+
+    }
+
+    public void addInManageTask(String idManageTask, String idTask){
+        this.manageTaskService.addInManageTask(idManageTask,idTask);
+    }
+
+    public void removeTask(String idManageTask, String idTask){
+        this.manageTaskService.removeTask(idManageTask,idTask);
+    }
+
+    public int countTasksInManage(String idManageTask){
+        return this.manageTaskService.countTasksInManage(idManageTask);
+    }
+}
